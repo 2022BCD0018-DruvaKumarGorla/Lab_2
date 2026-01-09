a624f507cdd6cf5dee7b654609b4769dd366a424
2022BCD0018_Druva_Kumar_Gorla <151651227+2022BCD0018-DruvaKumarGorla@users.noreply.github.com>
2026-01-09 23:18:59 +0530

    EXP-RF-03: Random Forest + Feature Selection

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ import joblib
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -12,41 +11,43 @@ from sklearn.metrics import mean_squared_error, r2_score
 # 1. Load Dataset
 df = pd.read_csv("dataset/winequality-red.csv", sep=';')
 
-# 2. Feature Selection
-X = df.drop("quality", axis=1)
+# 2. Feature Selection (manual)
+selected_features = [
+    "alcohol",
+    "sulphates",
+    "volatile acidity",
+    "citric acid"
+]
+
+X = df[selected_features]
 y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# 3. Standardization
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-# 4. Train Model
+# 3. Train Model
 model = RandomForestRegressor(
     n_estimators=100,
     random_state=42
 )
 model.fit(X_train, y_train)
 
-# 5. Evaluate
+# 4. Evaluate
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# 6. Save Outputs
+# 5. Save Outputs
 os.makedirs("outputs", exist_ok=True)
 joblib.dump(
-    {"model": model, "scaler": scaler},
+    {"model": model, "features": selected_features},
     "outputs/model.pkl"
 )
 
 with open("outputs/results.json", "w") as f:
     json.dump({"MSE": mse, "R2": r2}, f, indent=4)
 
-# 7. Print Metrics
+# 6. Print Metrics
 print(f"MSE: {mse}")
 print(f"R2 Score: {r2}")
