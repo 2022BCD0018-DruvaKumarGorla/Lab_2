999f73ed13818ae919ca7da2ad2c0692d0f2b37a
2022BCD0018_Druva_Kumar_Gorla <151651227+2022BCD0018-DruvaKumarGorla@users.noreply.github.com>
2026-01-09 22:53:14 +0530

    exp-01 baseline linear regression

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ import joblib
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -15,11 +14,6 @@ from sklearn.metrics import mean_squared_error, r2_score
 DATASET_PATH = "dataset/winequality-red.csv"
 df = pd.read_csv(DATASET_PATH, sep=';')
 
-# -----------------------------
-# 2. Preprocessing & Feature Selection
-# -----------------------------
-X = df.drop("quality", axis=1)
-y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
